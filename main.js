d12898c65442b12c407386c1cdf7054989b7593e
Artemisa Ruci <83644628+artemisaru@users.noreply.github.com>
2025-08-04 19:56:32 +0200

    style: add icon utils

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Svg sprite
+import.meta.glob('./assets/icons/*.svg', { eager: true });
+
 const toggleButton = document.getElementById('themeToggle');
 
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('theme-dark');
   document.body.classList.toggle('theme-light');
-})
+});
