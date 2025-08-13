3517f9458324685dd084c8525f8ec436d06be2ee
Artemisa Ruci <83644628+artemisaru@users.noreply.github.com>
2025-08-13 18:44:00 +0200

    feat: add custom cursor

--- scripts/customCursor.js
+++ scripts/customCursor.js
@@ -1,28 +1,45 @@
-export default function initCustomCursor(opts = {}) {
+export default function initCustomCursor() {
   const isFinePointer = window.matchMedia('(pointer: fine)').matches;
   const reduceMotion = window.matchMedia('(prefers-reduced-motion: reduce)').matches;
   // Don't init on touch or reduced motion
   if (!isFinePointer || reduceMotion) return;
 
-  console.log("What is this?")
+  // CONFIG
+  const CURSOR_CLASSES = {
+    active: 'active',
+    leaving: 'leaving',
+  }
 
+  const CURSOR_SIZES = {
+    default: { w: 24, h: 24 },
+    link_hover: { w: 24, h: 24 },
+  }
+
+  // DOM refs
   const cursor = document.querySelector('.custom-cursor');
+  const buttons = document.querySelectorAll('.btn--cursor');
+
+  // State variables
+  let mouseX = 0;
+  let mouseY = -100; // start off-screen
+  let width = CURSOR_SIZES.default.w;
+  let height = CURSOR_SIZES.default.h;
 
   // Track mouse
   function trackMouse(e) {
-    const mouseX = e.clientX;
-    const mouseY = e.clientY;
-
-    moveCursor(mouseX, mouseY);
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    updateCursorStyle();
   }
 
   // Move custom cursor
-  function moveCursor(x, y) {
-    const cursorX = x - cursor.offsetWidth / 2;
-    const cursorY = y - cursor.offsetHeight / 2;
-
-    cursor.style.transform = `translate(${cursorX}px, ${cursorY}px)`;
+  function updateCursorStyle() {
+    cursor.style.setProperty('--cursor-x', `${mouseX - width / 2}px`);
+    cursor.style.setProperty('--cursor-y', `${mouseY - height / 2}px`);
+    cursor.style.width = `${width}px`;
+    cursor.style.height = `${height}px`;
   }
 
-  document.addEventListener('mousemove', trackMouse)
+  // Event listeners
+  document.addEventListener('mousemove', trackMouse);
 }
