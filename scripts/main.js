1372259cba68968948d50f1b9abf6f9d0474d1b0
Artemisa Ruci <83644628+artemisaru@users.noreply.github.com>
2025-08-23 21:09:28 +0200

    feat: change swatch backgroundper theme

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,15 @@ initCustomCursor();
 // Svg sprite
 import.meta.glob('../assets/icons/*.svg', { eager: true });
 
+// Kit helpers
 const toggleButton = document.getElementById('themeToggle');
+const swatches = document.querySelectorAll('.kit-swatch--color');
 
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('theme-dark');
   document.body.classList.toggle('theme-light');
+  swatches.forEach(swatch => {
+    swatch.classList.toggle('swatch-dark');
+    swatch.classList.toggle('swatch-light');
+  })
 });
